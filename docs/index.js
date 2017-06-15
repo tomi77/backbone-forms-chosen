22982876b2cae5433e2520e9059ffc92230abd4f
Tomasz Jakub Rup <tomasz.rup@gmail.com>
2017-06-15 12:23:33 +0200

    Move templates to demo

---
+++ docs/index.js
@@ -0,0 +1,28 @@
+var demo;
+
+demo = function() {
+  var Model, form, model, states;
+  states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'];
+  Model = Backbone.Model.extend({
+    schema: {
+      states: {
+        type: 'chosen',
+        title: 'States',
+        options: states,
+        editorAttrs: {
+          multiple: 'multiple',
+          'data-placeholder': 'Select countries...'
+        }
+      }
+    }
+  });
+  model = new Model();
+  form = new Backbone.Form({
+    model: model
+  });
+  $('#form').html(form.render().$el);
+};
+
+document.addEventListener("DOMContentLoaded", demo, false);
+
+window.addEventListener("load", demo, false);
